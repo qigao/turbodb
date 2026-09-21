0bf527508c3a9bf3c45f82ccc6e8f3fb2eb8ac01
Qi Gao <qi.gao2012@gmail.com>
2026-09-21 16:56:21 +0800

    test(orm): verify storage descriptor native layout

--- orm/tests/driver/orm_driver_descriptor_layout_test.cpp
+++ orm/tests/driver/orm_driver_descriptor_layout_test.cpp
@@ -7,6 +7,7 @@
   static_assert(offsetof(T, header) == 0u, #T " prefix")
 LAYOUT(orm_driver_api_v1);
 LAYOUT(orm_driver_storage_capabilities_v1);
+LAYOUT(orm_driver_storage_capabilities_v1);
 LAYOUT(orm_driver_host_v1);
 LAYOUT(orm_driver_connection_v1);
 LAYOUT(orm_driver_transaction_v1);
